fad881fc74893c5f7801dae89c739ded0d1a3121
changdigx <changdix.gui@intel.com>
2019-04-17 16:44:20 +0800

    Add file

---
+++ train.py
@@ -0,0 +1,48 @@
+def init(data):
+    data['first'] = {}
+    data['middle'] = {}
+    data['last'] = {}
+
+def lookup(data,label,name):
+    return data[label].get(name)
+
+def store(data,full_name):
+    names = full_name.split()
+    if len(names) == 2 :
+        names.insert(1,"")
+    labels = 'first','middle','last'
+
+    for label,name in zip(labels,names):
+        people = lookup(data,label,name)
+        if people:
+            people.append(full_name)
+        else:
+            data[label][name] = [full_name]
+
+def print_params(x,y,z = 3, *pospar,**keypar):
+    print(x,y,z)
+    print(pospar)
+    print(keypar)
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def binarySearch(data,num,left = 0,right = None):
+    if right is None: right = len(data) - 1
+    if left >= right:
+        assert num == data[right]
+        return right
+    else:
+        middle = (left + right)//2
+        if num > data[middle]:
+            binarySearch(data,num,middle+1,right)
+        else:
+            binarySearch(data,num,left,middle)
+
+seq = [34,67,8,123,4,100,95]
+seq.sort()
+a = binarySearch(seq,34)
+print(a)
