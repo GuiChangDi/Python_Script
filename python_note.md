107acb361588f105da69b07d86cc629a0f958908
chandig <changdix.gui@intel.com>
2020-04-16 14:57:43 +0800

    Add python_automation train section 4,5

--- python_note.md
+++ python_note.md
@@ -1,1 +1,3 @@
 1.使用logging module来记录程序调试，而不要使用print.
+2.列表常见方法：len(),index(), append(), sort(), remove()
+3.字典常见方法：keys(), values(), items(), get(), setdefault()
--- python_train/train.py
+++ python_train/train.py
@@ -57,26 +57,3 @@ class SongBird(Bird):
         self.sound = 'Squawk'
     def sing(self):
         print(self.sound)
-
-#Section 4.1
-def Lamda(list):
-    list[len(list)-1] = 'and '+str(list[len(list)-1])
-    return ','.join(list)
-#Section 4.2
-grid = [['.', '.', '.', '.', '.', '.'],
-        ['.', '0', '0', '.', '.', '.'],
-        ['0', '0', '0', '0', '.', '.'],
-        ['0', '0', '0', '0', '0', '.'],
-        ['.', '0', '0', '0', '0', '0'],
-        ['0', '0', '0', '0', '0', '.'],
-        ['0', '0', '0', '0', '.', '.'],
-        ['.', '0', '0', '.', '.', '.'],
-        ['.', '.', '.', '.', '.', '.']]
-def Lamda1(list):
-    for i in range(len(list[0])):
-        for j in range(len(list)):
-            print(list[j][i], end='')
-        print('')
-
-if __name__ == '__main__':
-    Lamda1(grid)
