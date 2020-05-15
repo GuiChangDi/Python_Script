25f5d9072f1f3aa563783d413255532e1fc8d6b1
changdig <changdig@foxmail.com>
2020-05-15 14:42:47 +0800

    Add html.parser

--- book_example/downloadXkcd.py
+++ book_example/downloadXkcd.py
@@ -12,16 +12,16 @@ proxy = {
 while not url.endswith('#'):
     # Download the page.
     print('Downloading page %s...' %url)
-    res = requests.get(url, proxies = proxy)
+    res = requests.get(url)
     res.raise_for_status()
-    soup = bs4.BeautifulSoup(res.text)
+    soup = bs4.BeautifulSoup(res.text, 'feature = "html.parser"')
 
     # Find the URL of the comic image.
     comicElem = soup.select('#comic img')
     if comicElem == []:
         print('Could not find comic image.')
     else:
-        comicUrl = 'http' + comicElem[0].get('src')
+        comicUrl = 'http:' + comicElem[0].get('src')
         # Download the image.
         print('Downloading image %s...' % (comicUrl))
         res = requests.get(comicUrl)
